a5464c1a6a79385a2d4a097174fecf2af11e9a3b
Ashutosh Tiwari <ashutiw2k@gmail.com>
2025-04-08 02:35:44 -0400

    generate random circuits that can cause cycles without acting on their own controls.

--- helperfunctions/randomcircuit.py
+++ helperfunctions/randomcircuit.py
@@ -380,3 +380,126 @@ def random_quantum_circuit_varied_percentages(num_q=10, num_a=12, num_g=50,
             circuit.cx(in_q[i], ot_q[i])
         
     return circuit, num_q, num_a, num_g
+
+
+def random_quantum_circuit_type_2(num_d=10, num_a=12, num_g=50, 
+                                            add_init = True,  
+                                            add_outputs = False,
+                                            add_random_h = False,
+                                            percent_dd_gates = 0.8,
+                                            percent_aa_gates = 0.1,
+                                            percent_da_gates = 0.05,
+                                            percent_ad_gates = 0.05,
+                                            percent_add_h = 0.5) -> tuple[QuantumCircuit,int,int,int]:
+    '''
+    TODO - Will not work as expected. \\
+    This random circuit gen function ensures that the circuit generated has gates in which ancillae do not target their own controls.
+    Cycles are still possible if other ancillae change the controls before the ancilla can function. This restriction (should) be for 
+    data-ancilla and ancilla-data gates. Gates between data only and ancillae only can be of any fashion whatsoever. 
+    '''
+    # num_q = random.randint(3,10)
+    # num_a = random.randint(3,10)
+    # num_g = random.randint(50, 100)
+    # num_g = random.randint(10,50)
+    # num_g = 75
+    # print(f'{percent_cc_gates}+{percent_ca_gates}+{percent_ac_gates}+{percent_aa_gates} = {percent_cc_gates+percent_ca_gates+percent_ac_gates+percent_aa_gates}')
+    # assert percent_cc_gates+percent_ca_gates+percent_ac_gates+percent_aa_gates == 1.0
+
+    hc_gates = 0
+    dd_gates = 0
+    da_gates = 0
+    ad_gates = 0
+    aa_gates = 0
+    
+    da_q = QuantumRegister(num_d, name='dq')
+    an_q = QuantumRegister(num_a, name='aq')
+
+    if add_outputs:
+        ot_q = QuantumRegister(num_d, name='oq')
+        circuit = QuantumCircuit(da_q, ot_q, an_q)
+    else:
+        circuit = QuantumCircuit(da_q, an_q)
+
+    if add_init:
+        for q in da_q:
+            circuit.x(q)
+            circuit.h(q)
+    
+    rng = np.random.default_rng()
+    gate_dist = rng.random(size=num_g)
+    controls_dict = {q:list(circuit.qubits) for q in circuit.qubits}
+    [controls_dict[q].remove(q) for q in circuit.qubits]
+
+    print(controls_dict)
+
+    assert np.isclose(percent_aa_gates + percent_ad_gates + percent_da_gates + percent_dd_gates, 1)
+
+    for change_target_controls in tqdm(gate_dist, desc=f'Building Random Quantum Circuit with {num_d}d, {num_a}a, {num_g}g'):
+
+        if add_random_h:
+            if random.random() > percent_add_h:
+                wires = random.sample(list(da_q), random.randrange(num_d))
+                for w in wires:
+                    circuit.h(w)
+                    
+                hc_gates += len(wires) 
+        
+        # Default as control-control gates
+        control_q = da_q
+        target_q = da_q
+
+        if change_target_controls < percent_dd_gates:
+            control_q = da_q
+            target_q = da_q
+            dd_gates += 1
+
+        elif change_target_controls < percent_dd_gates + percent_da_gates:
+            control_q = da_q
+            target_q = an_q
+            da_gates += 1
+
+        elif change_target_controls < percent_dd_gates + percent_da_gates + percent_ad_gates:
+            control_q = an_q
+            target_q = da_q
+            ad_gates += 1
+
+        else:
+            control_q = an_q
+            target_q = an_q
+            aa_gates += 1
+            
+
+        # num_controls = 1 if control_q.size == 1 else random.randrange(1, control_q.size)
+        # target = random.randrange(target_q.size) # Get target qubit
+        # controls = random.sample(range(control_q.size), num_controls)  # Get control qubit/s
+        # # target = random.randrange(target_q.size) # Get target qubit
+        # if control_q == target_q:
+        #     target = random.randrange(target_q.size) # Get target qubit
+        #     valid_controls = list(range(control_q.size))
+        #     valid_controls.remove(target)
+        #     controls = random.sample(valid_controls, num_controls)  # Get control qubit/s
+        # else:
+        #     target = random.randrange(target_q.size) # Get target qubit
+        #     controls = random.sample(range(control_q.size), num_controls)  # Get control qubit/s
+        
+        # # print(num_controls, controls, target)
+        # circuit.mcx([control_q[cq] for cq in controls],target_q[target]) 
+
+        target = random.choice(target_q)
+        valid_controls = controls_dict[target]
+        control = random.choice(valid_controls)
+
+        if control_q != target_q:
+            controls_dict[control].remove(target)
+
+        circuit.cx(control, target)
+
+    logger.info(f'Built circuit with {num_d} input, {num_a} ancilla and {num_g} gates.')
+    logger.info(f'There are {hc_gates} H gates acting on the control qubits, {dd_gates} gates acting between control qubits, {da_gates} gates acting between control and ancilla, {ad_gates} gates acting between ancilla and control and {aa_gates} gates acting between just the ancillas.')
+    
+    if add_outputs:
+        for i in range(num_d):
+            circuit.cx(da_q[i], ot_q[i])
+        
+    return circuit, num_d, num_a, num_g
+
